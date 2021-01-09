f09a6fd22a9f07df12a5c77457a2a316be8acaab
Gina Beth Russell <ginabethrussell@Ginas-MBP.cinci.rr.com>
2021-01-09 11:51:59 -0500

    created form and formstate, created handlechange to set new form state, created handle submit to check form for errors and create error message in state, or invoke action creator to add new smurf to db

--- client/src/components/AddForm.js
+++ client/src/components/AddForm.js
@@ -1,24 +1,107 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addSmurf } from '../actions';
 
 class AddForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            position: '',
+            nickname: '',
+            description: '',
+            error: ''
+       }
+    }
 
+    handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({
+            ...this.state,
+            [name]: value})
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('submitting new smurf', this.state);
+        if (this.state.name === '' ){
+            this.setState({
+                ...this.state,
+                error: 'name must be included'
+            })
+        }else if(this.state.position === '' ){
+            this.setState({
+                ...this.state,
+                error: 'position must be included'
+            })
+        }else if (this.state.nickname === ''){
+            this.setState({
+                ...this.state,
+                error: 'nickname must be included'
+            })
+        }else {
+            const newSmurf = {
+                id: Date.now(),
+                name: this.state.name,
+                position: this.state.position,
+                nickname: this.state.nickname,
+                description: this.state.description
+            }
+            this.props.addSmurf(newSmurf);
+            this.setState({
+                name: '',
+                position: '',
+                nickname: '',
+                description: '',
+                error: ''
+            })
+        }
+        
+    }
     render() {
         return(<section>
             <h2>Add Smurf</h2>
             <form>
                 <div className="form-group">
                     <label htmlFor="name">Name:</label><br/>
-                    <input onChange={this.handleChange} name="name" id="name" />
+                    <input onChange={this.handleChange} 
+                    name="name" 
+                    id="name"
+                    value={this.state.name} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="position">Position:</label><br/>
+                    <input onChange={this.handleChange} 
+                    name="position" 
+                    id="position"
+                    value={this.state.position} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="nickname">Nickname:</label><br/>
+                    <input onChange={this.handleChange} 
+                    name="nickname" 
+                    id="nickname"
+                    value={this.state.nickname} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="description">Description:</label><br/>
+                    <input onChange={this.handleChange} 
+                    name="description" 
+                    id="description"
+                    value={this.state.description} />
                 </div>
 
-                <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: </div>
-                <button>Submit Smurf</button>
+
+                <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: {this.state.error}</div>
+                <button onClick={(e)=> this.handleSubmit(e)}>Submit Smurf</button>
             </form>
         </section>);
     }
 }
+const mapDispatchToProps = {addSmurf};
 
-export default AddForm;
+export default connect(null, mapDispatchToProps)(AddForm);
 
 //Task List:
 //1. Add in all necessary import components and library methods.
